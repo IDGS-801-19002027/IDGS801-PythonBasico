303c800f07789f0b7a5dcadfb7468d6c9ca309b8
@IDGS-801-19002027 <19002027@alumnos.utleon.edu.mx>
2023-01-26 17:52:46 -0600

    26 de enero del 2023

---
+++ actividad2.py
@@ -0,0 +1,48 @@
+import os
+
+class OperasBas:
+    def __init__(self, a, b):
+        self.n1 = a
+        self.n2 = b
+
+    def suma(self): 
+        self.res = self.n1 + self.n2
+        return self.res
+
+    def resta(self):
+        self.res = self.n1 - self.n2
+        return self.res
+
+    def multiplicacion(self):
+        self.res = self.n1 * self.n2
+        return self.res
+
+    def division(self): 
+        self.res = self.n1 / self.n2
+        return self.res
+
+def main():
+    opt = 5
+    while opt != -1:
+        
+        if (opt == 0):
+            break
+        else:
+            opt = int(input('1.- Suma\n 2.- Resta\n 3.- Multiplicación\n 4.- División\n 0.- Salir \n'))
+            n1 = int(input('Ingrese el primer número: '))
+            n2 = int(input('Ingrese el segundo número: '))
+            ob = OperasBas(n1,n2)
+            if (opt == 1):
+                res = ob.suma()
+            elif (opt == 2):
+                res = ob.resta()
+            elif (opt == 3):
+                res = ob.multiplicacion()
+            elif (opt == 4):
+                res = ob.division()
+            print('El resultado de la operación es: {}'.format(res))
+
+if __name__ == '__main__':
+    main()
+    
+  
